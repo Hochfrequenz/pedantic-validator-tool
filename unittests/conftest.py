b22c098ea045f872093b61f6c7ae5825e06cee8e
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2024-01-15 00:49:23 +0100

    📄

--- unittests/conftest.py
+++ unittests/conftest.py
@@ -1,5 +1,4 @@
-from collections.abc import Sequence
-from typing import Iterable, TypeVar
+from typing import TypeVar
 
 from pvframework.errors import ValidationError
 
@@ -8,39 +7,46 @@ BaseT = TypeVar("BaseT")
 
 
 def intersection_with_contains_str(
-    set_sub_container: Iterable[SubT], set_base_container: Iterable[BaseT]
-) -> tuple[list[SubT], list[BaseT]]:
+    set_sub_container: set[SubT], set_base_container: set[BaseT]
+) -> tuple[set[SubT], set[BaseT]]:
     """
-    Returns the intersection of two sets, but only if the intersection is not empty.
-    :param set_sub_container: the set to be checked for intersection
-    :param set_base_container: the set to be checked for intersection
-    :return: the intersection of the two sets, if the intersection is not empty
+    Calculates all elements from both sets "intersecting" in the following manner:
+    Let's call `set_sub_container` A and `set_base_container` B.
+    A consists of elements a1, a2, a3, ...
+    B consists of elements b1, b2, b3, ...
+
+    It returns a tuple two sets:
+    The first set contains all elements from A that are contained in at least one element of B.
+    I.e. {a_i ∈ A | ∃ b_j ∈ B : a_i ⊆ b_j}
+    LaTeX-Code: \left\{a_i \in A | \exists_{b_j \in B}: a_i \subseteq b_j\right\}
+    The second set contains all elements from B that have at least one element of A being a substring of the element
+    from B.
+    I.e. {b_j ∈ B | ∃ a_i ∈ A : b_j ⊇ a_i}
+    LaTeX-Code: \left\{b_j \in B | \exists_{a_i \in A}: b_j \supseteq a_i\right\}
     """
-    set_sub_container = list(set_sub_container)
-    set_base_container = list(set_base_container)
-    set_sub_container_intersection = []
-    set_base_container_intersection = []
-    for item_sub in set_sub_container:
-        set_base_remove_list = []
-        for item_base in set_base_container:
+    set_sub_container_intersection = set()
+    set_base_container_intersection = set()
+    for item_base in set_base_container:
+        for item_sub in set_sub_container:
             if str(item_sub) in str(item_base):
-                set_sub_container_intersection.append(item_sub)
-                set_base_container_intersection.append(item_base)
-                set_base_remove_list.append(item_base)
+                set_sub_container_intersection.add(item_sub)
+                set_base_container_intersection.add(item_base)
                 break
-        for item_base in set_base_remove_list:
-            set_base_container.remove(item_base)
 
     return set_sub_container_intersection, set_base_container_intersection
 
 
-def assert_full_error_coverage(expected_errors: Sequence[str], all_errors: Sequence[ValidationError]):
-    expected_errors_found, actual_errors_from_expected_list = intersection_with_contains_str(
-        expected_errors, all_errors
-    )
-    if len(expected_errors) != len(expected_errors_found) or len(all_errors) != len(actual_errors_from_expected_list):
-        expected_errors_not_found = list(set(expected_errors) - set(expected_errors_found))
-        uncovered_actual_errors = list(set(all_errors) - set(actual_errors_from_expected_list))
+def assert_full_error_coverage(expected_errors: set[str], all_errors: set[ValidationError]):
+    """
+    Asserts that all expected errors are found in the actual errors and vice versa.
+    The error messages in `expected_errors` don't have to be exact matches.
+    Every actual error message must have at least one element in `expected_errors` being
+    a substring of the actual error message.
+    """
+    expected_errors_found, actual_errors_in_expected_list = intersection_with_contains_str(expected_errors, all_errors)
+    if len(expected_errors) != len(expected_errors_found) or len(all_errors) != len(actual_errors_in_expected_list):
+        expected_errors_not_found = expected_errors - expected_errors_found
+        uncovered_actual_errors = all_errors - actual_errors_in_expected_list
         raise AssertionError(
             f"Expected errors not found: {expected_errors_not_found}\n"
             f"Actual errors not covered from expected list: {uncovered_actual_errors}"
